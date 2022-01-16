259a5a1a89871a4284fc0382c2fe32c36308d326
Karl Kuusik <karl32123@gmail.com>
2022-01-17 01:15:26 +0200

    added humidity logic

--- LogInstance.gd
+++ LogInstance.gd
@@ -25,6 +25,7 @@ var length = (end_position - start_position).y
 
 class DataNode extends Reference:
 	var position = Vector3(0.0,0.0,0.0)
+	var mesh
 	var stress = 0.0
 	var rot = 0.0
 
@@ -62,32 +63,49 @@ func genNodes():
 		i+=2
 	return nodes;
 
-func create_joint(pos, rotation_y, scale_vec, mesh):
+func create_joint(node, rotation_y, scale_vec, mesh, material):
+	var mesh_copy = mesh.duplicate()
+	var mat = material.duplicate()
+	if mat.get_shader_param("humidity"):
+		print("HAS MATERIAL HUMIDITY")
+		mat.set_shader_param("humidity", 0.0)
+	mesh_copy.surface_set_material(0, mat)
 	var end = MeshInstance.new()
-	end.translation = pos
+	end.translation = node.position
 	end.scale = scale_vec
-	end.set_mesh(mesh)
+	end.set_mesh(mesh_copy)
+	node.mesh = end
 	return end
 
-func create_end_joint(pos, rotation_y, isTop, scale_vec, mesh):
+func create_end_joint(node, rotation_y, isTop, scale_vec, mesh, material):
 	#var core_scale = Vector3(0.9, 0.05, 0.9);
-	if mesh == null: return;
+	if mesh == null: 
+		mesh = piece_mesh
+		var core_mesh = create_joint(node, rotation_y, scale_vec, mesh, bark_material)
+		self.add_child(core_mesh)
+		return
+		
 	var core_scale = Vector3(0.9, 1.0, 0.9);
-	var core_mesh = create_joint(pos, rotation_y, core_scale, mesh);
+	var core_mesh = create_joint(node, rotation_y, core_scale, mesh, material)
 	if isTop:
-		core_mesh.translation.y += 0.1
+		core_mesh.translation.y += 0.0 #used to be 0.1
 	else:
-		core_mesh.translation.y -= 0.1
+		core_mesh.translation.y -= 0.0 #used to be 0.1
 		core_mesh.rotate_x(PI)
 		core_mesh.rotate_y(PI)
 	self.add_child(core_mesh)
 
 func genTree(list):
 	var t_scale = Vector3(1.0, 1.0, 1.0);
-	create_end_joint(start_position, 0.0, false, t_scale, bottom_shape)
-	for node in list:
-		self.add_child(create_joint(node.position, 0.0, t_scale, piece_mesh))
-	create_end_joint(end_position, 0.0, true, t_scale, top_shape)
+	var i = 0
+	while i < len(list):
+		if i == 0:
+			create_end_joint(list[i], 0.0, false, t_scale, bottom_shape, end_material)
+		elif i+1 == len(list):
+			create_end_joint(list[i], 0.0, true, t_scale, top_shape, end_material)
+		else:
+			self.add_child(create_joint(list[i], 0.0, t_scale, piece_mesh, bark_material))
+		i += 1
 
 func getCollisionDisk(mesh, point, offset, boundary):
 	var returnable = []
@@ -124,7 +142,7 @@ func createEndHitbox(shape, pos, offset):
 	var hitbox_shape = ConvexPolygonShape.new()
 	var hitbox_points = []
 	hitbox_points += getCollisionDisk(shape, pos, offset, 1)
-	hitbox_points += getCollisionDisk(shape, pos, 0, 1)
+	hitbox_points += getCollisionDisk(shape, pos, offset-1, 1)
 	hitbox_shape.set_points(hitbox_points)
 	hitbox.shape = hitbox_shape
 	self.add_child(hitbox)
@@ -135,13 +153,18 @@ func createHitbox():
 	var hitbox_points = []
 	
 	if top_shape:
-		createEndHitbox(top_shape, end_position, 1)
-	
-	hitbox_points += getCollisionDisk(piece_mesh, end_position, 0, 1)
-	hitbox_points += getCollisionDisk(piece_mesh, start_position, 0, 1)
+		createEndHitbox(top_shape, end_position, 0)
+		var pos = Vector3(end_position.x, end_position.y - 1, end_position.z)
+		hitbox_points += getCollisionDisk(piece_mesh, pos, 0, 1)
+	else:
+		hitbox_points += getCollisionDisk(piece_mesh, end_position, 0, 1)
 	
 	if bottom_shape:
-		createEndHitbox(bottom_shape, start_position, -1)
+		createEndHitbox(bottom_shape, start_position, 1)
+		var pos = Vector3(end_position.x, start_position.y + 1, end_position.z)
+		hitbox_points += getCollisionDisk(piece_mesh, pos, 0, 1)
+	else:
+		hitbox_points += getCollisionDisk(piece_mesh, start_position, 0, 1)
 	
 	hitbox_shape.set_points(hitbox_points)
 	hitbox.shape = hitbox_shape
@@ -184,24 +207,54 @@ func on_aging_start(rate):
 	aging = true
 	aging_rate = rate
 
-func handleHumidity(node):
+func calcRot(prev_rot, current_rot):
+	if prev_rot < 0.35:
+		return 0.0
+	var operations_result = current_rot + 0.01
+	return clamp(operations_result, 0.0, 1.0)
+
+func handleHumidity():
 	# use translation for world position of node
 	# based on if touching ground, start increasing humidity from there.
-	return node
-
-func age():
 	var start_pos = translation + nodeList[0].position
 	var top_pos = translation + nodeList[len(nodeList)-1].position
-	if show_debug:
-		print(start_pos.y)
-		print(top_pos.y)
-		showPoints([nodeList[0].position, nodeList[len(nodeList)-1].position])
-		print("-----------------");
+	var diff = start_pos.y - top_pos.y
 	
+	#if show_debug:
+	#	print("Start: "+ str(start_pos.y))
+	#	print("Top: "+ str(top_pos.y))
+	#	print("-----------------")
+	var prev_rot = 0.35
 	var i = 0
-	while i < len(nodeList):
-		nodeList[i] = handleHumidity(nodeList[i])
-		i += 1
+	
+	if diff < 0.1: #top is higher
+		while i < len(nodeList):
+			var new_rot_value = calcRot(prev_rot, nodeList[i].rot)
+			nodeList[i].rot = new_rot_value
+			if show_debug:
+				nodeList[i].mesh.get_active_material(0).set_shader_param("humidity", new_rot_value)
+			prev_rot = new_rot_value
+			i += 1
+	elif diff > -0.1: #top is lower
+		i = len(nodeList) - 1
+		while i >= 0:
+			var new_rot_value = calcRot(prev_rot, nodeList[i].rot)
+			nodeList[i].rot = new_rot_value
+			if show_debug:
+				nodeList[i].mesh.get_active_material(0).set_shader_param("humidity", new_rot_value)
+			prev_rot = new_rot_value
+			i -= 1
+	else: # basically level
+		while i < len(nodeList):
+			var new_rot_value = calcRot(prev_rot, nodeList[i].rot)
+			nodeList[i].rot = new_rot_value
+			if show_debug:
+				nodeList[i].mesh.get_active_material(0).set_shader_param("humidity", new_rot_value)
+			prev_rot = new_rot_value
+			i += 1
+
+func age():
+	handleHumidity()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
--- interface/UI.gd
+++ interface/UI.gd
@@ -3,7 +3,7 @@ extends Control
 
 func on_aging_start():
 	print("ON AGING START")
-	Globals.emit_signal("start_aging", 0.5)
+	Globals.emit_signal("start_aging", 10.0)
 	pass
 
 # Called when the node enters the scene tree for the first time.
